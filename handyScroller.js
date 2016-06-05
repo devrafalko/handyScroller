9b2362fad665fd338310f71ca5a30d1d80f3458d
Paweł Rafałko <dev.rafalko@gmail.com>
2016-06-05 03:58:20 +0200

    All instructions assigning styles to elements replaced by function setStyles(). IE styling fixed.

--- handyScroller.js
+++ handyScroller.js
@@ -4,6 +4,7 @@ window.onload = function(){
 			side: "xy",
 			stretch:[true,false],
 			divideCorner:true,
+			wheelOrient:"horizontally",
 			wheelX:30,
 			scrollStep:[1,1]
 	});
@@ -66,13 +67,12 @@ function handyScroller(o){
 
 function createBoxes(){
 	var innerObj = this.mainBox.children;
-	var isStatic = window.getComputedStyle(this.mainBox,null).getPropertyValue("position")!=="static"? "":"position:relative;";
+	var isStatic = window.getComputedStyle(this.mainBox,null).getPropertyValue("position")!=="static"? ["position",null]:["position","relative"];
 
-	this.mainBox.style = "overflow:hidden;"+isStatic;
-	
+	setStyles(this.mainBox,["overflow",isStatic[0]],["hidden",isStatic[1]]);
 	this.contentBox = document.createElement("DIV");
 	this.contentBox.setAttribute("class","handyBox");
-	this.contentBox.style = "position:relative";
+	setStyles(this.contentBox,["position"],["relative"]);
 	for(;innerObj.length!==0;){
 		this.contentBox.appendChild(innerObj[0]);
 	}
@@ -87,9 +87,10 @@ function createScrolls(){
 	this.elements[this.xy][0].setAttribute("class","handyScrollBox"+this.stylesXY[this.xy][3]);
 	this.elements[this.xy][1].setAttribute("class","handyScrollArea"+this.stylesXY[this.xy][3]);
 	this.elements[this.xy][2].setAttribute("class","handyScroller"+this.stylesXY[this.xy][3]);
-	this.elements[this.xy][0].style = "position:absolute;"+this.stylesXY[this.xy][0]+":0px;"+this.stylesXY[this.xy][2]+":0px;"+this.stylesXY[this.xy][1]+":100%;box-sizing:border-box";
-	this.elements[this.xy][1].style= "position:relative;cursor:pointer;top:0px;left:0px;width:100%;height:100%;box-sizing:border-box;border:0;padding:0;margin:0";
-	this.elements[this.xy][2].style = "position:relative";
+	
+	setStyles(this.elements[this.xy][0],["position",this.stylesXY[this.xy][0],this.stylesXY[this.xy][2],this.stylesXY[this.xy][1],"boxSizing"],["absolute","0px","0px","100%","border-box"]);
+	setStyles(this.elements[this.xy][1],["position","cursor","top","left","width","height","boxSizing","border","padding","margin"],["relative","pointer","0px","0px","100%","100%","border-box","0","0","0"]);
+	setStyles(this.elements[this.xy][2],["position"],["relative"]);
 
 	this.elements[this.xy][1].appendChild(this.elements[this.xy][2]);
 	this.elements[this.xy][0].appendChild(this.elements[this.xy][1]);
@@ -111,13 +112,13 @@ function createScrolls(){
 		this.prepareToMove();
 		document.body.addEventListener("mouseup",releaseMe);
 		document.body.addEventListener("mousemove",scrollMove);
-		document.body.style.cursor = "pointer";
+		setStyles(document.body,["cursor"],["pointer"]);
 	}
 
 	function releaseMe(){
 		document.body.removeEventListener("mouseup",releaseMe);
 		document.body.removeEventListener("mousemove",scrollMove);
-		document.body.style.cursor = "default";
+		setStyles(document.body,["cursor"],["default"]);
 	}
 }
 
@@ -125,7 +126,7 @@ function createWheelBox(){
 	if((!this.isContentFit(1))&&(this.side!=="y")){
 		this.wheelBox = document.createElement("DIV");
 		this.wheelBox.setAttribute("class","handyWheelBox");
-		this.wheelBox.style = "position:absolute;right:0px;width:"+this.wheelX+"%;";
+		setStyles(this.wheelBox,["position","right","width"],["absolute","0px",this.wheelX+"%"]);
 		this.mainBox.insertBefore(this.wheelBox,this.elements[0][0]);
 	}	
 }
@@ -172,12 +173,12 @@ handyScroller.prototype.scrollMove = function(){
 function countMovements(pos){
 	var newPos = pos<0 ? 0:(pos+this.props[this.xy][9])>this.props[this.xy][8] ? this.props[this.xy][8]-this.props[this.xy][9]:pos;
 	var newPosProc = (newPos/this.props[this.xy][8])*100;
-	this.elements[this.xy][2].style[this.stylesXY[this.xy][0]] = newPosProc + "%";
+	setStyles(this.elements[this.xy][2],[[this.stylesXY[this.xy][0]]],[newPosProc + "%"]);
 	this.refreshMe();
 	
 	var scrollProc = (((this.props[this.xy][4]-this.props[this.xy][3])/(this.props[this.xy][8]-this.props[this.xy][9]))*100);
 	var boxProc = -(((this.props[this.xy][6]-this.props[this.xy][5])/this.props[this.xy][5])*scrollProc);
-	this.contentBox.style[this.stylesXY[this.xy][0]] = boxProc + "%";
+	setStyles(this.contentBox,[[this.stylesXY[this.xy][0]]],[boxProc + "%"]);
 }
 
 handyScroller.prototype.refreshMe = function(){
@@ -208,15 +209,17 @@ handyScroller.prototype.isContentFit = function(xy){
 
 handyScroller.prototype.stretchButton = function(){
 	if(this.isContentFit(this.xy)){
-		this.elements[this.xy][0].style.opacity = "0";
-		this.elements[this.xy][1].style.cursor = "default";
+		setStyles(this.elements[this.xy][0],["opacity"],["0"]);
+		setStyles(this.elements[this.xy][1],["cursor"],["default"]);
+		
+		
 		} else {
-			this.elements[this.xy][0].style.opacity = "1";
-			this.elements[this.xy][1].style.cursor = "pointer";
+			setStyles(this.elements[this.xy][0],["opacity"],["1"]);
+			setStyles(this.elements[this.xy][1],["cursor"],["pointer"]);
 			if(this.stretch[this.xy]){
-				this.elements[this.xy][2].style[this.stylesXY[this.xy][1]] = ((this.props[this.xy][5]/this.props[this.xy][6])*100) + "%";
+				setStyles(this.elements[this.xy][2],[[this.stylesXY[this.xy][1]]],[((this.props[this.xy][5]/this.props[this.xy][6])*100) + "%"]);
 				} else {
-					this.elements[this.xy][2].style[this.stylesXY[this.xy][1]] = null;
+					setStyles(this.elements[this.xy][2],[[this.stylesXY[this.xy][1]]],[null]);
 					}
 			}
 };
@@ -224,5 +227,11 @@ handyScroller.prototype.stretchButton = function(){
 handyScroller.prototype.setPaddings = function(){
 	this.scrollThick = this.divideCorner ? this.scrollThick:[0,0];
 	var paddingProc = 100-((this.scrollThick[this.xy]/this.props[this.xy][7])*100);
-	this.elements[this.xy][0].style[this.stylesXY[this.xy][1]] = paddingProc+"%";
+	setStyles(this.elements[this.xy][0],[[this.stylesXY[this.xy][1]]],[paddingProc+"%"]);
 };
+
+function setStyles(object,props,vals){
+	for(var i=0;i<props.length;i++){
+		object.style[props[i]] = vals[i];
+	}
+}
